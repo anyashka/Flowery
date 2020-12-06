5c231384116c6c7ec2f4badd1eeba0e8b23b1f55
Anna-Mariia Shkarlinska <anna-mariia.shkarlinska@netguru.com>
2020-12-06 11:09:38 +0100

    Create new project

---
+++ Flowery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flowery
+//
+//  Created by Anna-Mariia Shkarlinska on 06/12/2020.
+//  Copyright © 2020 Anna-Mariia Shkarlinska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
